711ee70b2551d2f002e1743eae6c9625e724bcfc
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-06-26 10:30:30 -0600

    Make add_mesaac_test more tuneable.

--- cmake_modules/add_mesaac_test.cmake
+++ cmake_modules/add_mesaac_test.cmake
@@ -6,17 +6,25 @@ file(REAL_PATH ${CMAKE_SOURCE_DIR}/tests/data TEST_DATA_DIR)
 # Any Catch2 `TEST_CASE` tags are added to the test as CMake test labels.
 # 
 # Arguments:
-# TEST_NAME - the name of the executable to build
-# SOURCES - the source files from which to build the executable
-#
+# TEST_NAME - name of the executable to build
+# SOURCES - source files from which to build the executable
+# LIBS - libraries against which to link the executable
 # Example:
 #   add_mesaac_test(foo foo.cpp foo_funcs.cpp)
-function(add_mesaac_test TEST_NAME)
-    set(SOURCES ${ARGN})
-    add_executable(${TEST_NAME} ${SOURCES})
-    target_compile_definitions(${TEST_NAME} PRIVATE TEST_DATA_DIR="${TEST_DATA_DIR}")
-    target_include_directories(${TEST_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/include)
-    target_link_libraries(${TEST_NAME} PRIVATE mesaac_shape mesaac_mol Catch2::Catch2WithMain)
+function(add_mesaac_test)
+    set(flags)
+    set(single_values TEST_NAME)
+    set(multi_values SOURCES LIBS)
+    cmake_parse_arguments(
+        arg
+        "${flags}" "${single_values}" "${multi_values}"
+        ${ARGN}
+    )
 
-    catch_discover_tests(${TEST_NAME} ADD_TAGS_AS_LABELS)
+    add_executable(${arg_TEST_NAME} ${arg_SOURCES})
+    target_compile_definitions(${arg_TEST_NAME} PRIVATE TEST_DATA_DIR="${TEST_DATA_DIR}")
+    target_include_directories(${arg_TEST_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/include)
+    target_link_libraries(${arg_TEST_NAME} PRIVATE ${arg_LIBS} Catch2::Catch2WithMain)
+
+    catch_discover_tests(${arg_TEST_NAME} ADD_TAGS_AS_LABELS)
 endfunction()
