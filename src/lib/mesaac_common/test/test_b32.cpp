9275ad274e0e29675847960316f33d9e9c622462
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-06-27 10:04:04 -0600

    Enable B32 and B64 tests.

--- src/lib/mesaac_common/test/test_b32.cpp
+++ src/lib/mesaac_common/test/test_b32.cpp
@@ -2,92 +2,65 @@
 // Copyright (c) 2008 Mesa Analytics & Computing, Inc.  All rights reserved
 //
 
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include <string>
-#include <iostream>
-#include <fstream>
+#include <random>
 #include <stdexcept>
-#include <cstdlib>
+#include <string>
 
-#include "mesaac_common/b32.h"
+#include "mesaac_common/b32.hpp"
 
 using namespace std;
 
-namespace mesaac 
-{
-    class TestCase : public CppUnit::TestFixture
-    {
-        CPPUNIT_TEST_SUITE(TestCase);
-        CPPUNIT_TEST(testBasic);
-        CPPUNIT_TEST(testFuzz);
-        CPPUNIT_TEST(testAllBytes);
-        
-        CPPUNIT_TEST_SUITE_END();
+namespace mesaac {
 
-    public:
-        void setUp() {
-        }
+namespace common {
 
-        void tearDown() {
-        }
-        
-        void roundtrip(string orig) {
-            B32 codec;
-            
-            string encoded = codec.encode(orig);
-            //cout << "Encoded " << encoded << endl;
-            string decoded = codec.decode(encoded);
-            CPPUNIT_ASSERT_EQUAL(orig, decoded);
-            //cout << "Round-trip string of length " << orig.size() << endl;
-        }
+namespace {
+void roundtrip(string orig) {
+  B32 codec;
 
-        void testBasic() {
-            string orig;
-            orig += (char)0xF0;
-            orig += (char)0x0F;
-            roundtrip(orig);
-        }
-        
-        void testFuzz() {
-            for (int i = 0; i < 160; i++) {
-                string src = "";
-                for (int j = 0; j < i; j++) {
-                    src += (char)(random() & 0xFF);
-                }
-                roundtrip(src);
-                CPPUNIT_ASSERT_EQUAL(src.size(), (size_t)i);
-            }
-        }
-        
-        void testAllBytes() {
-            for (int i = 0; i < 256; i++) {
-                string src = "";
-                for (int j = 0; j < 256; j++) {
-                    src += (char)((i + j) & 0xFF);
-                }
-                roundtrip(src);
-            }
-        }
-    };
-
-    CPPUNIT_TEST_SUITE_REGISTRATION(TestCase);
-};
-
-int
-main(int, char **)
-{
-    int result = 0;
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = 
-        CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest(registry.makeTest());
-    
-    if (!runner.run())
-    {
-        result = 1;
-    }
-    return result;
+  string encoded = codec.encode(orig);
+  string decoded = codec.decode(encoded);
+  REQUIRE(orig == decoded);
 }
+
+} // namespace
+
+TEST_CASE("mesaac::common::B32", "[mesaac]") {
+
+  SECTION("A basic test") {
+    string orig;
+    orig += (char)0xF0;
+    orig += (char)0x0F;
+    roundtrip(orig);
+  }
+
+  SECTION("Fuzz testing") {
+    // For reproducibility:
+    srandom(20250627);
+
+    for (int i = 0; i < 160; i++) {
+      string src = "";
+      for (int j = 0; j < i; j++) {
+        src += (char)(random() & 0xFF);
+      }
+      roundtrip(src);
+      REQUIRE(src.size() == i);
+    }
+  }
+
+  SECTION("Test with all byte values") {
+    for (int i = 0; i < 256; i++) {
+      string src = "";
+      for (int j = 0; j < 256; j++) {
+        src += (char)((i + j) & 0xFF);
+      }
+      roundtrip(src);
+    }
+  }
+}
+
+} // namespace common
+}; // namespace mesaac
--- src/lib/mesaac_common/test/test_b64.cpp
+++ src/lib/mesaac_common/test/test_b64.cpp
@@ -2,124 +2,104 @@
 // Copyright (c) 2010 Mesa Analytics & Computing, Inc.  All rights reserved
 //
 
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include <string>
-#include <iostream>
-#include <fstream>
+#include <random>
 #include <stdexcept>
-#include <cstdlib>
+#include <string>
 
-#include "mesaac_common/b64.h"
+#include "mesaac_common/b64.hpp"
 
 using namespace std;
 
-namespace mesaac 
-{
-    class TestCase : public CppUnit::TestFixture
-    {
-        CPPUNIT_TEST_SUITE(TestCase);
-        CPPUNIT_TEST(testBasic);
-        CPPUNIT_TEST(testFuzz);
-        CPPUNIT_TEST(testAllBytes);
-        CPPUNIT_TEST(testCorrupted);
+namespace mesaac {
+namespace common {
 
-        CPPUNIT_TEST_SUITE_END();
+namespace {
+void roundtrip(string orig) {
+  B64 codec;
 
-        void roundtrip(string orig) {
-            B64 codec;
-            
-            string encoded = codec.encode(orig);
-            // cout << "Encoded: " << encoded << endl;
-            string decoded = codec.decode(encoded);
-            CPPUNIT_ASSERT_EQUAL(orig, decoded);
-            //cout << "Round-trip string of length " << orig.size() << endl;
-        }
-
-        void testBasic() {
-            string orig;
-            orig += (char)0xF0;
-            orig += (char)0x0F;
-            roundtrip(orig);
-        }
-        
-        void testFuzz() {
-            for (int i = 0; i < 160; i++) {
-                string src = "";
-                for (int j = 0; j < i; j++) {
-                    src += (char)(random() & 0xFF);
-                }
-                roundtrip(src);
-                CPPUNIT_ASSERT_EQUAL(src.size(), (size_t)i);
-            }
-        }
-        
-        void testAllBytes() {
-            for (int i = 0; i < 256; i++) {
-                string src = "";
-                for (int j = 0; j < 256; j++) {
-                    src += (char)((i + j) & 0xFF);
-                }
-                roundtrip(src);
-            }
-        }
-        
-        char randomChar() {
-            // Get a random character which is not a valid B64 char.
-            static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-            char result = '_';
-            if ((random() & 0xFF) >= 0x10) {
-                const int MAX = (int)('+') - 1;
-                result = (char)(random() % MAX);
-            } else {
-                const int MIN = (int)('z') + 1;
-                const int MAX = 255;
-                result = (char)(MIN + (random() % (MAX - MIN)));
-            }
-            return result;
-        }
-        
-        void testCorrupted() {
-            // Fuzz test, decoding corrupted B64 strings.
-            // In this case "corrupted" means only that the string
-            // contains at least one invalid B64 character.
-            B64 codec;
-            const int LENGTH = 160;
-            
-            for (int i = 1; i < LENGTH; i++) {
-                string src = "";
-                for (int j = 0; j < i; j++) {
-                    src += (char)(random() & 0xFF);
-                }
-            
-                string encoded = codec.encode(src);
-                // Append or replace a single character of the encoded
-                // string w. an invalid B64 character.
-                int byte_to_munge = (random() % i);
-                encoded[byte_to_munge] = randomChar();
-                CPPUNIT_ASSERT_THROW(codec.decode(encoded), std::invalid_argument);
-            }
-        }
-        
-    };
-
-    CPPUNIT_TEST_SUITE_REGISTRATION(TestCase);
-};
-
-int
-main(int, char **)
-{
-    int result = 0;
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = 
-        CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest(registry.makeTest());
-    
-    if (!runner.run())
-    {
-        result = 1;
-    }
-    return result;
+  string encoded = codec.encode(orig);
+  string decoded = codec.decode(encoded);
+  REQUIRE(orig == decoded);
 }
+
+char random_char() {
+  // Get a random character which is not a valid B64 char.
+  static string alphabet =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  char result = '_';
+  if ((random() & 0xFF) >= 0x10) {
+    const int MAX = (int)('+') - 1;
+    result = (char)(random() % MAX);
+  } else {
+    const int MIN = (int)('z') + 1;
+    const int MAX = 255;
+    result = (char)(MIN + (random() % (MAX - MIN)));
+  }
+  return result;
+}
+
+} // namespace
+
+TEST_CASE("mesaac::common::B64", "[mesaac]") {
+
+  SECTION("Basic test") {
+    string orig;
+    orig += (char)0xF0;
+    orig += (char)0x0F;
+    roundtrip(orig);
+  }
+
+  SECTION("Fuzz testing") {
+    // For reproducibility:
+    srandom(20250627);
+    for (int i = 0; i < 160; i++) {
+      string src = "";
+      for (int j = 0; j < i; j++) {
+        src += (char)(random() & 0xFF);
+      }
+      roundtrip(src);
+      REQUIRE(src.size() == i);
+    }
+  }
+
+  SECTION("Test with all byte values") {
+    for (int i = 0; i < 256; i++) {
+      string src = "";
+      for (int j = 0; j < 256; j++) {
+        src += (char)((i + j) & 0xFF);
+      }
+      roundtrip(src);
+    }
+  }
+
+  SECTION("Decode corrupted B64 strings") {
+    // Fuzz test, decoding corrupted B64 strings.
+    // In this case "corrupted" means only that the string
+    // contains at least one invalid B64 character.
+    B64 codec;
+    const int LENGTH = 160;
+
+    // For reproducibility:
+    srandom(20250627);
+
+    for (int i = 1; i < LENGTH; i++) {
+      string src = "";
+      for (int j = 0; j < i; j++) {
+        src += (char)(random() & 0xFF);
+      }
+
+      string encoded = codec.encode(src);
+      // Append or replace a single character of the encoded
+      // string w. an invalid B64 character.
+      int byte_to_munge = (random() % i);
+      encoded[byte_to_munge] = random_char();
+      REQUIRE_THROWS_AS(codec.decode(encoded), std::invalid_argument);
+    }
+  }
+}
+
+} // namespace common
+} // namespace mesaac
