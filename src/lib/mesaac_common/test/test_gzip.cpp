a2e9020e6cdbbc40297c92319f93c33db0079c4e
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-06-27 10:26:57 -0600

    Enable gzip test.

--- src/lib/mesaac_common/test/test_gzip.cpp
+++ src/lib/mesaac_common/test/test_gzip.cpp
@@ -2,78 +2,58 @@
 // Copyright (c) 2010 Mesa Analytics & Computing, Inc.  All rights reserved
 //
 
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <string>
 
-#include "mesaac_common/gzip.h"
+#include "mesaac_common/gzip.hpp"
 
 using namespace std;
 
-namespace mesaac 
-{
-    class TestCase : public CppUnit::TestFixture
-    {
-        CPPUNIT_TEST_SUITE(TestCase);
-        CPPUNIT_TEST(testRoundTrip);
-        CPPUNIT_TEST(testFuzz);
-        // CPPUNIT_TEST(testCorrupted);
-        
-        CPPUNIT_TEST_SUITE_END();
+namespace mesaac {
+namespace common {
 
-    protected:
-        void roundtrip(string orig) {
-            string compressed = gzip::compress(orig);
-            string decompressed = gzip::decompress(compressed);
-            CPPUNIT_ASSERT_EQUAL(orig, decompressed);
-        }
-
-    public:
-        void testRoundTrip() {
-            string original("Some test, huh.");
-            roundtrip(original);
-        }
-
-        void testFuzz() {
-            for (int i = 0; i < 160; i++) {
-                string src = "";
-                for (int j = 0; j < i; j++) {
-                    src += (char)(random() & 0xFF);
-                }
-                roundtrip(src);
-                CPPUNIT_ASSERT_EQUAL(src.size(), (size_t)i);
-            }
-        }
-        
-        // void testCorrupted() {
-        //     // Too hard to test for corruption using random manglings.
-        //     // If compressed data is corrupted, then sometimes you'll get
-        //     // a runtime_error.  Sometimes you'll get no error, but an
-        //     // inconsistent result.  Sometimes you'll get a correct result.
-        // }
-    };
-
-    CPPUNIT_TEST_SUITE_REGISTRATION(TestCase);
-};
-
-int
-main(int, char **)
-{
-    int result = 0;
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = 
-        CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest(registry.makeTest());
-    
-    if (!runner.run())
-    {
-        result = 1;
-    }
-    return result;
+namespace {
+void roundtrip(string orig) {
+  string compressed = gzip::compress(orig);
+  string decompressed = gzip::decompress(compressed);
+  REQUIRE(orig == decompressed);
 }
+
+} // namespace
+
+TEST_CASE("mesaac::common::gzip", "[mesaac]") {
+
+  SECTION("Basic round-trip") {
+    string original("Some test, huh.");
+    roundtrip(original);
+  }
+
+  SECTION("Fuzz test") {
+    srandom(20250627);
+    for (int i = 0; i < 160; i++) {
+      string src = "";
+      for (int j = 0; j < i; j++) {
+        src += (char)(random() & 0xFF);
+      }
+      roundtrip(src);
+      REQUIRE(src.size() == i);
+    }
+  }
+
+  // SECTION("Fuzzy corruption") {
+  //     // Too hard to test for corruption using random manglings.
+  //     // If compressed data is corrupted, then sometimes you'll get
+  //     // a runtime_error.  Sometimes you'll get no error, but an
+  //     // inconsistent result.  Sometimes you'll get a correct result.
+  // }
+}
+
+} // namespace common
+} // namespace mesaac
