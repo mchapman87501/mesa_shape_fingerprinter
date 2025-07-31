167d0540c41704e0290eec0517463db05ea502d4
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-07-31 07:48:21 -0600

    Reformat some doc comments.

--- src/lib/mesaac_mol/include/mesaac_mol/io/sdreader.hpp
+++ src/lib/mesaac_mol/include/mesaac_mol/io/sdreader.hpp
@@ -15,26 +15,33 @@ namespace mesaac::mol {
 
 class SDReaderImpl;
 
-/// @brief SDReader reads Mol instances from an SD file.
+/**
+ * @brief SDReader reads Mol instances from an SD file.
+ */
 class SDReader {
 public:
-  /// @brief Create an SD file reader.
-  /// @param inf stream from which to read SD structures
-  /// @param pathname if given, the pathname from which `inf` is reading
+  /**
+   * @brief Create an SD file reader.
+   * @param inf stream from which to read SD structures
+   * @param pathname if given, the pathname from which `inf` is reading
+   */
   SDReader(std::istream &inf, const std::string &pathname = "(input stream)");
 
   ~SDReader();
 
-  /// @brief Skip the next molecule.
-  /// @return true if the reader was able to skip ahead
+  /**
+   * @brief Skip the next molecule.
+   * @return true if the reader was able to skip ahead
+   */
   bool skip();
 
-  /// @brief Read the next molecule/structure.
-  /// @param mol on successful return, the next read molecule
-  /// @return whether or not a molecule could be read into `mol`
-  ///
-  /// @note If the return value is `false`, then `mol` will be an empty
-  /// molecule.
+  /**
+   * @brief Read the next molecule/structure.
+   * @param mol on successful return, the next read molecule
+   * @return whether or not a molecule could be read into `mol`
+   *
+   * @note If the return value is `false`, then `mol` will be an empty molecule.
+   */
   bool read(Mol &mol);
 
 private:
