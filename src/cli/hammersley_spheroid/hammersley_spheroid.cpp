11b9b93d4cd2e5854c02d0ce328bd3ae4decb592
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-08-08 10:46:45 -0600

    Preallocate points.  Compute 3 dimensions, not 5.

--- src/cli/hammersley_spheroid/hammersley_spheroid.cpp
+++ src/cli/hammersley_spheroid/hammersley_spheroid.cpp
@@ -7,7 +7,7 @@
 // i/N, and where succeeding dimensions are van der Corput sequences generated
 // with distinct prime bases.  The first dimension uses 2 as its prime number
 // and is done in a separate for loop for performance.  The remaining dimensions
-// make calls to the "base_function" to generate the van der Corput sequence for
+// make calls to the "radix_digits" to generate the van der Corput sequence for
 // a specific prime base.  The benefits of quasi-random sequences of the
 // Hammersly sequence begins to break down at around 40 dimensions without some
 // fixes (not implemented here). Thus, the dimensions are capped at for now at
@@ -17,38 +17,39 @@
 
 #include <array>
 #include <bitset>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <libgen.h>
-#include <string>
 #include <vector>
 
 #include "mesaac_arg_parser/arg_parser.hpp"
 
 using namespace std;
+
+using Point3D = array<float, 3>;
+using PointVec = vector<Point3D>;
+
+// In practice only the first of these primes is used.
 static constexpr array<unsigned int, 29> primes{
     3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37,  41,  43,  47,
     53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109};
 
-vector<unsigned int> base_function(unsigned int prime, unsigned int number) {
+// Get the digits of a number n, base radix, in order from least significant to
+// most significant.
+vector<unsigned int> radix_digits(const unsigned int n,
+                                  const unsigned int radix) {
   vector<unsigned int> base_vector;
-  unsigned int quotient = number;
+  unsigned int quotient = n;
 
   while (quotient > 0) {
-    base_vector.push_back(quotient % prime);
-    quotient /= prime;
+    base_vector.push_back(quotient % radix);
+    quotient /= radix;
   }
   return base_vector;
 };
 
 void generate_points(const unsigned int sample_size, const float scale,
-                     vector<vector<float>> &result) {
-  vector<unsigned int> base_vector;
-  vector<float> a_dimension;
+                     PointVec &result) {
+  result.resize(sample_size);
+  result.clear();
 
   // represent the integers i = 1,...,K as binary
   // multiply least significant bit by 1 plus its binary place reciprocal
@@ -57,19 +58,18 @@ void generate_points(const unsigned int sample_size, const float scale,
   // Store indices in a k length vector of ints.
 
   // Store first dimension, the sequence i/N (sample_size)
-  for (unsigned int i = 1; i <= sample_size; i++) {
-    a_dimension.push_back((float)i / float(sample_size));
+  size_t i_dim = 0;
+  for (size_t i = 0; i < sample_size; i++) {
+    result[i][i_dim] = (float)(i + 1) / float(sample_size);
   }
-  result.push_back(a_dimension);
-  a_dimension.clear();
 
   // van der Corput second dimension with 2 as the first prime.  Faster than
   // the remaining prime dimensions
-
-  for (unsigned int i = 1; i <= sample_size; i++) {
-    unsigned int j = 0;
+  i_dim += 1;
+  for (size_t i = 1; i <= sample_size; i++) {
+    size_t j = 0;
     double sum = 0.0;
-    unsigned int twocount = 1;
+    size_t twocount = 1;
     bitset<32> i_int(i);
     while (twocount <= i) {
       if (i_int[j]) {
@@ -78,53 +78,43 @@ void generate_points(const unsigned int sample_size, const float scale,
       j++;
       twocount *= 2;
     }
-    a_dimension.push_back(sum);
+    result[i - 1][i_dim] = sum;
   }
 
-  result.push_back(a_dimension);
-  a_dimension.clear();
-
-  // Remaining dimensions are successive primes 3, 5, 7, ...
-  unsigned int k = 0;
-  while (k < 3) {
-    unsigned int prime = primes[k];
-    for (unsigned int i = 1; i <= sample_size; i++) {
-      unsigned int j = 0;
-      double sum = 0.0;
-      unsigned int count = 1;
-      base_vector = base_function(prime, i);
-      while (count <= i) {
-        if (base_vector[j]) {
-          sum += base_vector[j] / (float)(count * prime);
-        }
-        j++;
-        count *= prime;
+  i_dim += 1;
+  const unsigned int prime = primes[0];
+  for (size_t i = 1; i <= sample_size; i++) {
+    size_t j = 0;
+    float sum = 0.0;
+    size_t count = 1;
+    const auto base_vector = radix_digits(i, prime);
+    while (count <= i) {
+      if (base_vector[j]) {
+        sum += base_vector[j] / (float)(count * prime);
       }
-      a_dimension.push_back(sum);
+      j++;
+      count *= prime;
     }
-    result.push_back(a_dimension);
-    a_dimension.clear();
-    k++;
+    result[i - 1][i_dim] = sum;
   }
 
   // Center at origin (0,0,0) and scale
   for (unsigned int i = 0; i < sample_size; i++) {
     for (unsigned int j = 0; j < 3; j++) {
-      result[j][i] = scale * (1.0 - 2.0 * result[j][i]);
+      result[i][j] = scale * (1.0 - 2.0 * result[i][j]);
     }
   }
 }
 
-void output_points(ostream &outs, const vector<vector<float>> &points,
+void output_points(ostream &outs, const PointVec &points,
                    unsigned int sample_size, const float a, const float b,
                    const float c, const float scale) {
   const double scale_sqr = scale * scale;
   for (unsigned int i = 0; i < sample_size; i++) {
-    if ((((points[0][i]) * (points[0][i])) / a +
-         ((points[1][i]) * (points[1][i])) / b +
-         ((points[2][i]) * (points[2][i])) / c) < scale_sqr) {
-      outs << points[0][i] << " " << points[1][i] << " " << points[2][i]
-           << endl;
+    const Point3D &point(points[i]);
+    if (((point[0] * point[0]) / a + (point[1] * point[1]) / b +
+         (point[2] * point[2]) / c) < scale_sqr) {
+      outs << point[0] << " " << point[1] << " " << point[2] << endl;
     }
   }
 }
@@ -152,8 +142,7 @@ int main(int argc, const char **const argv) {
 
   // TODO extra cmdline arg validation.
 
-  unsigned int i, j, twocount;
-  vector<vector<float>> points;
+  PointVec points;
 
   generate_points(sample_size->value(), scale->value(), points);
 
