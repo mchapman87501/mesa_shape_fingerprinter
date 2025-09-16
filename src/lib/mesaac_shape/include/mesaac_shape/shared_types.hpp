438b4ee68cdec5bce82a86f4279218da282ecb60
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-09-16 12:57:23 -0600

    Clarify meanings of types.

--- src/lib/mesaac_shape/include/mesaac_shape/shared_types.hpp
+++ src/lib/mesaac_shape/include/mesaac_shape/shared_types.hpp
@@ -20,7 +20,12 @@ using Point = std::vector<float>;
 using PointList = std::vector<Point>;
 
 /**
- * @brief Represents a single shape (conformer) for a single orientation.
+ * @brief Indicates which points in a point intersect with a single orientation
+ * of a conformer.
+ * @details A Fingerprint is a sequence of bits, each corresponding to a single
+ * point in a quasi-random point set.  A Fingerprint is meant to represent a
+ * conformer in a single spatial orientation.  In such a Fingerprint, a bit is
+ * non-zero if the corresponding point intersects the volume of the conformer.
  */
 using Fingerprint = shape_defs::BitVector;
 
@@ -30,13 +35,13 @@ using Fingerprint = shape_defs::BitVector;
 using FingerprintVector = shape_defs::ArrayBitVectors;
 
 /**
- * @brief A sequence of fingerprints for a single shape (conformer),
+ * @brief A sequence of Fingerprints for a single shape (conformer),
  * each of which represents the shape in a single "canonical" orientation.
  */
 using ShapeFingerprint = shape_defs::ArrayBitVectors;
 
 /**
- * @brief A ShapeFingerprintVector holds a collection of shape fingerprints.
+ * @brief A collection of ShapeFingerprints.
  */
 using ShapeFingerprintVector = shape_defs::ShapeFPBlocks;
 } // namespace mesaac::shape
