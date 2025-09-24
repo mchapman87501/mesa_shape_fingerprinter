da20380bbb5ec4e819326963af828364b067c5e7
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-09-24 11:18:09 -0600

    Add SDWriter benchmarks.

--- tests/src/lib/mesaac_mol/io/benchmark_write_mols.cpp
+++ tests/src/lib/mesaac_mol/io/benchmark_write_mols.cpp
@@ -7,6 +7,7 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include "mesaac_mol/io/sdreader.hpp"
+#include "mesaac_mol/io/sdwriter.hpp"
 
 namespace mesaac::mol {
 namespace {
@@ -16,26 +17,40 @@ const std::filesystem::path test_sdf_path(const std::string &rel_path) {
   return sd_files_dir / rel_path;
 }
 
-void read_and_discard(const std::string &rel_path) {
+void read_and_write(const std::string &rel_path) {
+  // TODO: disentangle reading from writing.
+  // OBS this tests writing to a string stream but does not verify
+  // the output is as expected.
   const auto pathname = test_sdf_path(rel_path);
   std::ifstream inf(pathname);
   mesaac::mol::SDReader reader(inf, pathname);
+
+  std::ostringstream outs;
+  mesaac::mol::SDWriter writer(outs);
+
   while (true) {
     auto result = reader.read();
     if (!result.is_ok()) {
       return;
     }
+    writer.write(result.value());
+  }
+
+  if (outs.str().empty()) {
+    FAIL("Did not write anything for " + rel_path);
   }
 }
 
-TEST_CASE("mesaac::mol::SDReader Benchmarks", "[mesaac][mesaac_benchmark]") {
+TEST_CASE("mesaac::mol::SDWriter Benchmarks", "[mesaac][mesaac_benchmark]") {
 
-  BENCHMARK_ADVANCED("read VS 2000")(Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { read_and_discard("cox2_3d_first_5.sd"); });
+  BENCHMARK_ADVANCED("read/write VS 2000")(
+      Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { read_and_write("cox2_3d_first_5.sd"); });
   };
 
-  BENCHMARK_ADVANCED("read VS 3000")(Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { read_and_discard("v3000_with_coords_first_5.sdf"); });
+  BENCHMARK_ADVANCED("read/write VS 3000")(
+      Catch::Benchmark::Chronometer meter) {
+    meter.measure([] { read_and_write("v3000_with_coords_first_5.sdf"); });
   };
 }
 
