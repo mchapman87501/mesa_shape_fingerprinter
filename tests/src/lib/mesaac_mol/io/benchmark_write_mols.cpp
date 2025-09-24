193f1f8faafefeb7d9c04c8b63d35acbd3b7bf2e
Mitch Chapman <mchapman87501@users.noreply.github.com>
2025-09-24 11:46:42 -0600

    Exclude mol reads from benchmark.

--- tests/src/lib/mesaac_mol/io/benchmark_write_mols.cpp
+++ tests/src/lib/mesaac_mol/io/benchmark_write_mols.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
@@ -17,40 +18,48 @@ const std::filesystem::path test_sdf_path(const std::string &rel_path) {
   return sd_files_dir / rel_path;
 }
 
-void read_and_write(const std::string &rel_path) {
-  // TODO: disentangle reading from writing.
-  // OBS this tests writing to a string stream but does not verify
-  // the output is as expected.
+void read_mols(const std::string &rel_path, std::vector<Mol> &mols) {
+  mols.clear();
   const auto pathname = test_sdf_path(rel_path);
   std::ifstream inf(pathname);
   mesaac::mol::SDReader reader(inf, pathname);
-
-  std::ostringstream outs;
-  mesaac::mol::SDWriter writer(outs);
 
   while (true) {
     auto result = reader.read();
     if (!result.is_ok()) {
       return;
     }
-    writer.write(result.value());
+    mols.emplace_back(std::move(result.value()));
   }
+}
 
-  if (outs.str().empty()) {
-    FAIL("Did not write anything for " + rel_path);
+void write_mols(const std::vector<Mol> &mols) {
+  std::ostringstream outs;
+  mesaac::mol::SDWriter writer(outs);
+  for (const auto &mol : mols) {
+    writer.write(mol);
   }
+  CHECK(!outs.str().empty());
 }
 
 TEST_CASE("mesaac::mol::SDWriter Benchmarks", "[mesaac][mesaac_benchmark]") {
 
-  BENCHMARK_ADVANCED("read/write VS 2000")(
+  BENCHMARK_ADVANCED("write from VS 2000 input")(
       Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { read_and_write("cox2_3d_first_5.sd"); });
+    std::vector<Mol> mols;
+    read_mols("cox2_3d_first_5.sd", mols);
+    CHECK(!mols.empty());
+
+    meter.measure([&mols] { write_mols(mols); });
   };
 
-  BENCHMARK_ADVANCED("read/write VS 3000")(
+  BENCHMARK_ADVANCED("write from VS 3000 input")(
       Catch::Benchmark::Chronometer meter) {
-    meter.measure([] { read_and_write("v3000_with_coords_first_5.sdf"); });
+    std::vector<Mol> mols;
+    read_mols("v3000_with_coords_first_5.sdf", mols);
+    CHECK(!mols.empty());
+
+    meter.measure([&mols] { write_mols(mols); });
   };
 }
 
